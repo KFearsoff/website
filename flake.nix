336bb1ba2bcb5a9a325bda3be653fa0e075b93ed
KFears <kfearsoff@gmail.com>
2024-10-16 22:51:33 +0400

    feat: update flake

--- flake.nix
+++ flake.nix
@@ -1,71 +1,57 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    devenv.url = "github:cachix/devenv";
-    nix2container.url = "github:nlewo/nix2container";
-    nix2container.inputs.nixpkgs.follows = "nixpkgs";
-    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
-
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [
-        inputs.devenv.flakeModule
-      ];
-      systems = ["x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
-
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        ...
-      }: {
-        devenv.shells.default = {
-          languages = {
-            nix.enable = true;
+  outputs =
+    inputs@{ nixpkgs, pre-commit-hooks, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      checks."x86_64-linux" = {
+        pre-commit-check = pre-commit-hooks.lib."x86_64-linux".run {
+          src = ./.;
+          hooks = {
+            # Nix
+            nixfmt-rfc-style.enable = true;
+            deadnix.enable = true;
+            statix.enable = true;
           };
-
-          # https://github.com/cachix/devenv/issues/528
-          containers = pkgs.lib.mkForce {};
-
-          packages = with pkgs; [
-            alejandra
-            mdbook
-            mdbook-mermaid
-            mdbook-admonish
-          ];
         };
-
-        packages.default = with pkgs;
-          stdenv.mkDerivation {
-            pname = "nixalted-website";
-            version = "0.1.0";
-
-            src = ./.;
-
-            buildInputs = [
-              mdbook
-              mdbook-mermaid
-              mdbook-admonish
-            ];
-
-            buildPhase = ''
-              mdbook build
-              cp -r book $out
-            '';
-          };
       };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
+
+      devShells."x86_64-linux".default = pkgs.mkShellNoCC {
+        packages = [
+          pkgs.mdbook
+          pkgs.mdbook-mermaid
+          pkgs.mdbook-admonish
+        ];
+
+        inherit (inputs.self.checks.x86_64-linux.pre-commit-check) shellHook;
+
+      };
+
+      packages."x86_64-linux".default = pkgs.stdenv.mkDerivation {
+        pname = "nixalted-website";
+        version = "0.1.0";
+
+        src = ./.;
+
+        buildInputs = [
+          pkgs.mdbook
+          pkgs.mdbook-mermaid
+          pkgs.mdbook-admonish
+        ];
+
+        buildPhase = ''
+          mdbook build
+          cp -r book $out
+        '';
       };
     };
 }
